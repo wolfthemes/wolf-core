a1a42d46c6106919f2ef20a5d415b458c9c8076f
WolfThemes <contact@wolfthemes.com>
2022-04-21 10:18:16 +0200

    added new theme compat

--- inc/admin/auth.php
+++ inc/admin/auth.php
@@ -245,6 +245,7 @@ function wolf_core_wrong_theme() {
 		'spectacle',
 		'effigy',
 		'omnity',
+		'elsass',
 	);
 
 	return ( ! in_array( esc_attr( sanitize_title_with_dashes( get_template() ) ), $ok, true ) );
