fa2b0f79760b1bcc7c5020b151df19fc00085003
GitHub Action <action@github.com>
2026-05-04 08:43:47 +0000

    Release 2.1.20

---
+++ inc/admin/admin-wpb-vc-functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Wolf Core WPBakery Page Builder admin functions
+ *
+ * Admin functions specific to WPB VC
+ *
+ * @author WolfThemes
+ * @category Admin
+ * @package WolfCore/Admin
+ * @version 1.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
---
+++ plugins/elements/blank.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Blank
+ *
+ * @author WolfThemes
+ * @package WolfCore/Elements
+ * @version 1.0.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Returns the element markup
+ *
+ * @param array $atts The element attributes.
+ */
+function wolf_core_blank( $atts ) {
+
+	$atts = apply_filters(
+		'wolf_core_blank_atts',
+		wp_parse_args(
+			$atts,
+			array()
+		)
+	);
+
+	extract( $atts ); // phpcs:ignore
+
+	$output = '';
+
+	$class = $el_class; // init container CSS class.
+
+	return $output;
+}
